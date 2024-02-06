19506ac86eb09bde5d23e3878ab57969f3b5c5a9
Osipov Andrei <osipovdop@mail.ru>
2024-02-06 09:02:16 +0300

    Initial

---
+++ src/Animal.java
@@ -0,0 +1,14 @@
+public class Animal {
+    public int id;
+    public String name;
+
+    public Animal(){}
+
+    public Animal(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+    public void eat(){
+        System.out.println("Animal is eating...");
+    }
+}
