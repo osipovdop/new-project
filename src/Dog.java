97be271071b4cd6e31a2327898178c5dc3e4ba3f
Osipov Andrei <osipovdop@mail.ru>
2024-02-06 09:16:41 +0300

    Deleted "run" method.

--- src/Dog.java
+++ src/Dog.java
@@ -2,9 +2,6 @@ public class Dog extends Animal{
     public void bark(){
         System.out.println("Dog is barking...");
     }
-    public static void run(){
-        System.out.println("Dog is running...");
-    }
     public void eat(){
         System.out.println("Dog is eating...");
     }
