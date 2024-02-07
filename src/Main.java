967ccef5e6fbefea31ad53f89c5967e6d23790f5
osipovdop <105889599+osipovdop@users.noreply.github.com>
2024-02-07 04:24:28 +0300

    Update Main.java
    333

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         Animal animal = new Animal();
-        int[] massiv = {1,2,5,3,212,53,0,32,1,54,4};
+        int[] massiv = {1,2,5,3,212,53,0,32,1,54,8};
 
     }
 }
