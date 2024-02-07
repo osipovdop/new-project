b2f8b5c01e6ffc8d4f5720aac8b919ba113b5d18
Osipov Andrei <osipovdop@mail.ru>
2024-02-07 07:27:08 +0300

    Новый метод в классе Animal

--- src/Animal.java
+++ src/Animal.java
@@ -11,4 +11,7 @@ public class Animal {
     public void eat(){
         System.out.println("Animal is eating...");
     }
+    public void run(){
+        System.out.println("Animal is running...");
+    }
 }
