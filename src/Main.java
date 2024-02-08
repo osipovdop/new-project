113a909a94c21d43ed1604c78bafdcfbbfc6e1c9
Osipov Andrei <osipovdop@mail.ru>
2024-02-08 06:31:33 +0300

    8

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,11 @@ public class Main {
     public static void main(String[] args) {
         Animal animal = new Animal();
 
+<<<<<<< Updated upstream
         int[] massiv = {1,2,5,3,212,53};
+        int x = 11;
+=======
+        int[] massiv = {1,2,5,3,212,53,8,2,21,33};
+>>>>>>> Stashed changes
     }
 }
