308a7fe98335733d09da191cef6d8bfafd07cc47
Osipov Andrei <osipovdop@mail.ru>
2024-02-08 06:53:31 +0300

    0

--- src/Dog.java
+++ src/Dog.java
@@ -2,7 +2,5 @@ public class Dog extends Animal{
     public void bark(){
         System.out.println("Dog is barking...");
     }
-    public void eat(){
-        System.out.println("Dog is eating...");
-    }
+    
 }
