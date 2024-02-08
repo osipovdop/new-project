2ab48c51aa83c5bc0bd85c55cd0ade1c02dd9a73
Osipov Andrei <osipovdop@mail.ru>
2024-02-08 06:44:20 +0300

    0

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         Animal animal = new Animal();
-        int[] massiv = {1,2,5,3,212,53,3,11,25,61};
-        int x = 3;
+
     }
 }
