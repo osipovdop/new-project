497ea3dec54b3eca1b27650358bf7127af317c00
Osipov Andrei <osipovdop@mail.ru>
2024-02-08 10:02:23 +0300

    8

--- src/Dog.java
+++ src/Dog.java
@@ -2,5 +2,8 @@ public class Dog extends Animal{
     public void bark(){
         System.out.println("Dog is barking...");
     }
+    public void eat(){
+        System.out.println("Dog is eating/...");
+    }
     
 }
--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         Animal animal = new Animal();
         int[] numbers = {0,1,2,3,4,5,6};
+        int index = 35;
     }
 }
